1de3539d29fa484bc2e791d9feb41baf08cd5458
mohitsh <mohitsh114@gmail.com>
2015-10-10 04:14:09 +0530

    amexp.R

--- amexp.R
+++ amexp.R
@@ -52,8 +52,17 @@ EducBack <- as.factor(x[[22]])
 x$mvar_22 <- as.character(x$mvar_22)
 x$mvar_22[x$mvar_22 == "professiol"] <- "professional"
 x$mvar_22 <- as.factor(x$mvar_22)
-
+RallyCent <- as.numeric(x[[23]])
+RallyCent[is.na(RallyCent)] <- mean(RallyCent,na.rm=TRUE)
+RallyEbo <- as.numeric(x[[24]])
+RallyEbo[is.na(RallyEbo)] <- mean(RallyEbo,na.rm=TRUE)
+RallyTok <- as.numeric(x[[25]])
+RallyTok[is.na(RallyTok)] <- mean(RallyTok,na.rm=TRUE)
+RallyOdy <- as.numeric(x[[26]])
+RallyOdy[is.na(RallyOdy)] <- mean(RallyOdy,na.rm=TRUE)
 Rally <- as.numeric(x[[27]])
+RallyCos <- as.numeric(x[[28]])
+RallyCos[is.na(RallyCos)] <- mean(RallyCos,na.rm=TRUE)
 Rally[is.na(Rally)] <- mean(Rally, na.rm=TRUE)
 income <- as.numeric(x[[30]])
 income[is.na(income)] <- mean(income,na.rm=TRUE)
@@ -62,7 +71,8 @@ income[is.na(income)] <- mean(income,na.rm=TRUE)
 train_data = data.frame(y,partyVoted,DonaCent,DonaEbo,DonaTok,DonaOdy,DonaCos,
                         ShareCent,ShareEbo,ShareTok,ShareOdy,ShareCos,
                         Occup,RegCode,HouseHold,ageBucket,married,HomeOwn,PolitAffi,
-                        ResidingYears,PerVoted,PartiesVoted,EducBack,Rally,income)
+                        ResidingYears,PerVoted,PartiesVoted,EducBack,RallyCent,
+                        RallyEbo,RallyTok,RallyOdy,Rally,RallyCos,income)
 
 train_data$base <- relevel(train_data$y, ref = "CENTAUR")
 
@@ -72,9 +82,13 @@ train_data$base <- relevel(train_data$y, ref = "CENTAUR")
 #                ResidingYears+PerVoted+PartiesVoted+EducBack+Rally+income, data = train_data)
 
 
-fit <- multinom(base ~ partyVoted+
-                        Occup+RegCode+HouseHold+ageBucket+married+HomeOwn+PolitAffi+
-                        ResidingYears+PerVoted+PartiesVoted+EducBack+Rally, data = train_data)
+fit <- multinom(base ~ partyVoted+DonaCent+DonaEbo+DonaTok+
+                DonaOdy+DonaCos+
+                ShareCent+ShareEbo+ShareTok+ShareOdy+
+                ShareCos+
+                        Occup+RegCode+ageBucket+married+HomeOwn+
+                        +PerVoted+EducBack+
+                        + RallyCent+RallyEbo+RallyTok+RallyOdy+Rally+RallyCos, data = train_data)
 
 
 #z <- summary(fit)$coefficients/summary(fit)$standard.errors
@@ -114,8 +128,22 @@ ResidingYears <- as.numeric(x_pred[[19]])
 PerVoted <- as.numeric(x_pred[[20]])
 PartiesVoted <- as.numeric(x_pred[[21]])
 EducBack <- as.factor(x_pred[[22]])
+
+RallyCent <- as.numeric(x_pred[[23]])
+RallyCent[is.na(RallyCent)] <- mean(RallyCent,na.rm=TRUE)
+RallyEbo <- as.numeric(x_pred[[24]])
+RallyEbo[is.na(RallyEbo)] <- mean(RallyEbo,na.rm=TRUE)
+RallyTok <- as.numeric(x_pred[[25]])
+RallyTok[is.na(RallyTok)] <- mean(RallyTok,na.rm=TRUE)
+RallyOdy <- as.numeric(x_pred[[26]])
+RallyOdy[is.na(RallyOdy)] <- mean(RallyOdy,na.rm=TRUE)
+
+
 Rally <- as.numeric(x_pred[[27]])
 Rally[is.na(Rally)] <- mean(Rally, na.rm=TRUE)
+RallyCos <- as.numeric(x_pred[[28]])
+RallyCos[is.na(RallyCos)] <- mean(RallyCos,na.rm=TRUE)
+
 income <- as.numeric(x_pred[[30]])
 income[is.na(income)] <- mean(income,na.rm=TRUE)
 
@@ -131,11 +159,15 @@ income[is.na(income)] <- mean(income,na.rm=TRUE)
 #                  EducBack,Rally,income)
 
 
-data = data.frame(partyVoted,
-                  Occup,RegCode,HouseHold,ageBucket,
-                  married,HomeOwn,PolitAffi,
-                  ResidingYears,PerVoted,PartiesVoted,
-                  EducBack,Rally)
+data = data.frame(partyVoted,DonaCent,DonaEbo,DonaTok,
+                  DonaOdy,DonaCos,
+                  ShareCent,ShareEbo,ShareTok,ShareOdy,
+                  ShareCos,                        
+                  Occup,RegCode,ageBucket,
+                  married,HomeOwn,
+                  PerVoted,
+                  EducBack,RallyCent,
+                  RallyEbo,RallyTok,RallyOdy,Rally,RallyCos)
 
 
 prediction <- predict(fit,newdata = data,"probs")
@@ -146,4 +178,5 @@ prediction <- as.data.frame(prediction)
 #prediction <- prediction[c("CENTAUR","COSMOS","EBONY","TOKUGAWA")]
 
 prediction$FinalVote <- colnames(prediction)[apply(prediction,1,which.max)]
-write.table(prediction,file="dude5.csv",sep=",")
+write.table(prediction,file="dude7.csv",sep=",")
+
